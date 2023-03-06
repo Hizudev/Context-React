6af38ff4fbee92fe089ab8d477176f1e935f3dcb
NicoHndzHc <nico.hndz.s@gmail.com>
2023-03-06 06:01:27 -0300

    4to commit

--- src/context/ProyectContext.jsx
+++ src/context/ProyectContext.jsx
@@ -1,23 +1,9 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export const ProyectContext = createContext();
 
 export const Provider = ({ children }) => {
   const [data, setData] = useState([]);
-
-  const getData = async () => {
-    try {
-      const response = await fetch("../../public/fotos.json");
-      const apiData = await response.json();
-      setData(apiData.photos);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <ProyectContext.Provider value={{ data, setData }}>
