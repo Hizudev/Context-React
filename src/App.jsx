176de4df6145d5489badccf5c32ae14d6de99255
NicoHndzHc <nico.hndz.s@gmail.com>
2023-03-06 20:30:58 -0300

    Commit final

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import ResponsiveAppBar from "./components/Navbar";
-import { useContext, useEffect } from "react";
-import {ProyectContext} from "./context/ProyectContext"
 
 const App = () => {
-  const { setData } = useContext(ProyectContext);
-  const getData = async () => {
-    try {
-      const response = await fetch('../public/fotos.json');
-      const apiData = await response.json();
-      setData(apiData.photos);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <>
       <ResponsiveAppBar />
--- src/context/ProyectContext.jsx
+++ src/context/ProyectContext.jsx
@@ -1,9 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ProyectContext = createContext();
 
 export const Provider = ({ children }) => {
   const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch("/fotos.json");
+      const apiData = await response.json();
+      setData(apiData.photos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <ProyectContext.Provider value={{ data, setData }}>
